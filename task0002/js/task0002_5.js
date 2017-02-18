95c67b354ddb9cc864b5402a60122b9fad164774
ltaoo <litaowork@aliyun.com>
2017-02-18 15:59:34 +0800

    【优化】task0002 小练习5 准确定位放置位置

--- task0002/js/task0002_5.js
+++ task0002/js/task0002_5.js
@@ -2,12 +2,22 @@ document.body.onload = function () {
     var sourceList = document.querySelectorAll('#source .container__item')
     // 拿到目标容器
     var targetContainer = document.querySelector('#target')
+    // 定义一个变量用来保存在目标容器内已经有多少个 item
+    var targetList = []
     // 目标容器在屏幕上的坐标
     var targetContainerX = targetContainer.offsetLeft
     var targetContainerY = targetContainer.offsetTop
+    // 拿到 item 的宽高
+    var cell = document.querySelector('#source .container__item')
+    var cellWidth = cell.clientWidth
+    var cellHeight = cell.clientHeight
 
     // 定义一个全局变量用来标志是否进入了容器，默认 false
     var enterContainer = false
+    // 定义一个全局变量用来标志应该插入到哪个位置
+    var _index = 0
+
+    var virtualNode
 
     /* sourceList.addEventListener('drag', function (event) {
      *     console.log(event.target.innerHTML)
@@ -38,7 +48,6 @@ document.body.onload = function () {
             node.style.backgroundColor = '#fff'
             node.style.border = '2px dotted #ccc'
 
-
             // console.log(x, y)
 
             if(x > targetContainerX) {
@@ -47,6 +56,30 @@ document.body.onload = function () {
             } else {
                 enterContainer = false
                 targetContainer.style.border = '1px solid #ccc'
+            }
+
+            // 疯狂计算位置
+            if(enterContainer) {
+                // 如果进入了容器，必定会有这个虚拟节点
+                // 一个在目标容器内的虚拟节点用来预知位置
+                if(!virtualNode) {
+                    virtualNode = document.createElement('li')
+                    virtualNode.className = 'container__item--virtual'
+                }
+                // 还应该加上 targetContaner 的 top 值
+                if(y >= targetList.length*cellHeight) {
+                    targetContainer.appendChild(virtualNode)
+                }
+                else {
+                    // 计算出现在鼠标应该是在哪个位置
+                    var index = Math.floor(y/cellHeight)
+                    targetContainer.insertBefore(virtualNode, (targetContainer.children)[index])
+                }
+            } else {
+                if(virtualNode) {
+                    targetContainer.removeChild(virtualNode)
+                }
+                virtualNode = null
             }
         }
         // 结束拖拽
@@ -58,7 +91,14 @@ document.body.onload = function () {
             node.style.borderBottom = '1px solid #ccc'
 
             if(enterContainer) {
-                targetContainer.appendChild(node)
+                // targetContainer.appendChild(node)
+                targetContainer.replaceChild(node, virtualNode)
+                targetList.push(node)
+                // 移除虚拟节点
+                /* if(virtualNode) { */
+                    // targetContainer.removeChild(virtualNode)
+                /* } */
+                virtualNode = null
             }
             node.style.cursor = 'pointer'
             targetContainer.style.border = '1px solid #ccc'
