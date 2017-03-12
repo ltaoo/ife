a6c29d7ced8b7feb1f05fd8a4bd760bdb78e4602
ltaoo <litaowork@aliyun.com>
2017-03-12 22:35:21 +0800

    【实现】自定义 emitter

---
+++ fakeVue/Emitter.js
@@ -0,0 +1,50 @@
+// 尝试自己实现一个事件监听发布机制
+function Emitter () {
+	this.events = {}
+	return this
+}
+
+/* 事件监听函数
+ * @param <String> event 要监听的事件名
+ * @param <Function> fn  事件触发时要调用的函数
+ */
+Emitter.prototype.on = function (event, fn) {
+	// 由于支持注册同名事件，所以事件对应的函数肯定是数组
+	var fns = this.events[event] || []
+	fns.push(fn)
+	this.events[event] = fns
+}
+
+/* 事件触发函数
+ * @param <String> event 要触发的事件名
+ */
+Emitter.prototype.emit = function (event) {
+	var fns = this.events[event]
+
+	fns.forEach(fn => {
+		fn()
+	})
+}
+
+/* 取消指定事件监听
+ * @param <String> event
+ * @param:options <Function> fn
+ */
+Emitter.prototype.off = function (event, fn) {
+	// 首先判断该事件是否存在
+	var fns = this.events[event]
+	// if ([]) true or false ? answer is true
+	if (!fns || fns.length === 0) {
+		console.log('没有注册该事件')
+		return
+	}
+	// 如果没有传 fn ，就取消该事件所有的函数，否则只取消这一个
+	if (fn) {
+		var index = fns.indexOf(fn)
+		fns.splice(index, 1)
+	} else {
+		fns = []
+	}
+
+	this.events[event] = fns
+}
