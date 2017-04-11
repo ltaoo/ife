d7bfecd4e0eee94f48ccacb1959865db561b1408
ltaoo <litaowork@aliyun.com>
2017-04-11 14:02:50 +0800

    【实现】同时支持 opacity 和 width 改变

--- uiComponents/task17/src/fakeVelocity.js
+++ uiComponents/task17/src/fakeVelocity.js
@@ -10,8 +10,9 @@
     }
     // 分割值与单位
     function separateValue (value) {
-        // 假设要改变的值没有单位，比如 opacity
-        return [value, '']
+        // 只处理两种简单的情况，没有单位和单位为 px
+        const result = /([0-9]+)([a-z]{2})?/.exec(value)
+        return [result[1], result[2] || '']
     }
 
     /* ========================
@@ -33,32 +34,34 @@
         const opts = {
             duration: 400
         }
-        let startSeparatedValue,
-            startValue,
-            unitType,
-            endSeparatedValue,
-            endValue,
-            property // 简单处理的情况，只改变一个属性值，后面这个肯定要删除
-        for(property in propertiesMap) {
-            startSeparatedValue = separateValue(getPropertyValue(element, property))
+        // 保存要改变的属性集合
+        let propertiesContainer = {}
+        for(let property in propertiesMap) {
+            const startSeparatedValue = separateValue(getPropertyValue(element, property))
             // 拿到开始值
-            startValue = startSeparatedValue[0]
+            const startValue = startSeparatedValue[0]
             // 单位值
-            unitType = startSeparatedValue[1]
+            const unitType = startSeparatedValue[1]
             // 先处理简单的情况，假设只能改变一个属性
-            endSeparatedValue = separateValue(propertiesMap[property])
+            const endSeparatedValue = separateValue(propertiesMap[property])
             // 结束值
-            endValue = endSeparatedValue[0]
+            const endValue = endSeparatedValue[0]
+            // 将上面的值都保存到 propertiesContainer 中 参考源码 1812
+            propertiesContainer[property] = {
+                startValue,
+                endValue,
+                unitType,
+                currentValue: startValue
+            }
         }
         let timeStart
-        // 属性当前值，将会逐渐变化，直到变成 endValue
-        let currentValue = startValue
         // 终止动画标志
         let isTicking = true
         // 核心动画函数
         function tick () {
             // 当前时间
             let timeCurrent = (new Date).getTime()
+            // 遍历要执行动画的 element 元素，这里暂时只支持一个元素
             // 当前值
             // 如果 timeStart 是 undefined ，表示这是动画的第一次执行
             if (!timeStart) {
@@ -66,21 +69,28 @@
             }
             // 检测动画是否执行完毕
             const percentComplete = Math.min((timeCurrent - timeStart) / opts.duration, 1) 
-            // 如果动画执行完成
-            if (percentComplete === 1) {
-                currentValue = endValue
-            } else {
-                currentValue = parseFloat(startValue) + ((endValue - startValue) * Animation.easing['swing'](percentComplete))
-            }
-            // 改变 dom 的属性值
-            setPropertyValue(element, property, currentValue + unitType)
-            // 终止调用 tick
-            if (percentComplete === 1) {
-                isTicking = false
-            }
 
-            if (isTicking) {
-                requestAnimationFrame(tick)
+            // 遍历要改变的属性值并一一改变
+            for(let property in propertiesContainer) {
+                // 拿到该属性当前值，一开始是 startValue
+                const tween = propertiesContainer[property]
+                // 如果动画执行完成
+                if (percentComplete === 1) {
+                    currentValue = tween.endValue
+                } else {
+                    currentValue = parseFloat(tween.startValue) + ((tween.endValue - tween.startValue) * Animation.easing['swing'](percentComplete))
+                    tween.currentValue = currentValue
+                }
+                // 改变 dom 的属性值
+                setPropertyValue(element, property, currentValue + tween.unitType)
+                // 终止调用 tick
+                if (percentComplete === 1) {
+                    isTicking = false
+                }
+
+                if (isTicking) {
+                    requestAnimationFrame(tick)
+                }
             }
         }
 
