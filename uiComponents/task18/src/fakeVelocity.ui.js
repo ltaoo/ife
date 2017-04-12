ff853873f821aeca78dcc485981bf85478e9b95d
ltaoo <litaowork@aliyun.com>
2017-04-12 21:56:07 +0800

    【完成】简单的预定义动效

--- uiComponents/task18/src/fakeVelocity.ui.js
+++ uiComponents/task18/src/fakeVelocity.ui.js
@@ -6,6 +6,75 @@
         return
     }
 
+    // 预定义动画
+    const effects = {
+        "transition.slideUpIn": {
+            defaultDuration: 900,
+            calls: [
+                [{opacity: [1, 0], translateY: [0, 20], translateZ: 0}]
+            ]
+        },
+        "transition.slideUpOut": {
+            defaultDuration: 900,
+            calls: [
+                [{opacity: [0, 1], translateY: -20, translateZ: 0}]
+            ],
+            reset: {translateY: 0}
+        },
+        "transition.slideDownIn": {
+            defaultDuration: 900,
+            calls: [
+                [{opacity: [1, 0], translateY: [0, -20], translateZ: 0}]
+            ]
+        },
+        "transition.slideDownOut": {
+            defaultDuration: 900,
+            calls: [
+                [{opacity: [0, 1], translateY: 20, translateZ: 0}]
+            ],
+            reset: {translateY: 0}
+        },
+        "transition.slideLeftIn": {
+            defaultDuration: 1000,
+            calls: [
+                [{opacity: [1, 0], translateX: [0, -20], translateZ: 0}]
+            ]
+        },
+        "transition.slideLeftOut": {
+            defaultDuration: 1050,
+            calls: [
+                [{opacity: [0, 1], translateX: -20, translateZ: 0}]
+            ],
+            reset: {translateX: 0}
+        },
+        "transition.slideRightIn": {
+            defaultDuration: 1000,
+            calls: [
+                [{opacity: [1, 0], translateX: [0, 20], translateZ: 0}]
+            ]
+        },
+        "transition.slideRightOut": {
+            defaultDuration: 1050,
+            calls: [
+                [{opacity: [0, 1], translateX: 20, translateZ: 0}]
+            ],
+            reset: {translateX: 0}
+        }
+    }
+
+    Animation.prototype.effect = function (effectName, options={}) {
+        // 先拿到动画对应的值
+        console.log(effects, effectName)
+        if (effects[effectName]) {
+            const properties = effects[effectName]
+            let opts = Object.assign(options, {
+                duration: properties.defaultDuration
+            })
+            const propertiesMap = properties.calls[0][0]
+            this.animation(propertiesMap, opts)
+        }
+    }
+
     Animation.runSequence = function (originalSequence) {
         const sequence = [...originalSequence]
         if (sequence.length > 1) {
