2e0c6747ebc56e2e3ece904704da8571de58099d
ltaoo <litaowork@aliyun.com>
2017-04-13 14:00:44 +0800

    【增加】一些简单的缓动动画

--- uiComponents/task18/src/fakeVelocity.js
+++ uiComponents/task18/src/fakeVelocity.js
@@ -148,6 +148,26 @@
         Animation.easing = {
             swing: function (a) {
                 return .5 - Math.cos(a * Math.PI) / 2
+            },
+            Sine: function (p) {
+                return 1 - Math.cos(p * Math.PI / 2);
+            },
+            Circ: function (p) {
+                return 1 - Math.sqrt(1 - p * p);
+            },
+            Elastic: function(p) {
+                return p === 0 || p === 1 ? p :
+                    -Math.pow(2, 8 * (p - 1)) * Math.sin(((p - 1) * 80 - 7.5) * Math.PI / 15);
+            },
+            Back: function(p) {
+                return p * p * (3 * p - 2);
+            },
+            Bounce: function (p) {
+                var pow2,
+                    bounce = 4;
+
+                while (p < ((pow2 = Math.pow(2, --bounce)) - 1) / 11) {}
+                return 1 / Math.pow(4, 3 - bounce) - 7.5625 * Math.pow((pow2 * 3 - 2) / 22 - p, 2);
             }
         }
         // 暴露的动画接口
