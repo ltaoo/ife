7c6766aee96d91e746ef404dbd2749587e5b1372
ltaoo <litaowork@aliyun.com>
2017-04-14 22:19:44 +0800

    【实现】单例模式

--- module/module.js
+++ module/module.js
@@ -1,4 +1,5 @@
-function CoolModule () {
+// 将函数声明并定义修改为立即执行函数，作用是相同的，但是无法多次调用 CoolModule 方法返回多个“模块”
+var foo = (function CoolModule () {
     var something = 'cool'
 
     var another = [1, 2, 3]
@@ -14,9 +15,8 @@ function CoolModule () {
         doSomething,
         doAnother
     }
-}
+})()
 
-var foo = CoolModule()
 
 foo.doSomething()
 foo.doAnother()
