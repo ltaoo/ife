2540e501788f4542468e79fc1e3fe04055449489
ltaoo <litaowork@aliyun.com>
2017-04-14 22:31:26 +0800

    【实现】简单的现代模块机制

--- module/module.js
+++ module/module.js
@@ -1,22 +1,49 @@
-// 将函数声明并定义修改为立即执行函数，作用是相同的，但是无法多次调用 CoolModule 方法返回多个“模块”
-var foo = (function CoolModule () {
-    var something = 'cool'
+var MyModules = (function Manager () {
+    var modules = {}
 
-    var another = [1, 2, 3]
-
-    function doSomething () {
-        console.log(something)
+    function define (name, deps, impl) {
+        for (var i = 0; i < deps.length; i++) {
+            deps[i] = modules[deps[i]]
+        }
+        // 核心，将函数保存在了 modules 内
+        modules[name] = impl.apply(impl, deps)
     }
-    function doAnother () {
-        console.log(another.join('!'))
+
+    function get (name) {
+        return modules[name]
     }
 
     return {
-        doSomething,
-        doAnother
+        define: define,
+        get: get
     }
 })()
 
+// 定义模块
+MyModules.define('bar', [], function () {
+    function hello (who) {
+        return 'Let me introduce: ' + who
+    }
 
-foo.doSomething()
-foo.doAnother()
+    return {
+        hello: hello
+    }
+})
+MyModules.define('foo', ['bar'], function (bar) {
+    var hungry = 'hippo'
+
+    function awesome () {
+        console.log(bar.hello(hungry).toUpperCase())
+    }
+
+    return {
+        awesome: awesome
+    }
+})
+
+// OK，从这里可以看到，由于这是一个闭包，所以MyModules 可以给 foo 对应
+// 的函数需要的东西比如这里的 bar
+
+// 所以 foo 可以使用 bar 的方法
+var foo = MyModules.get('foo')
+foo.awesome()
