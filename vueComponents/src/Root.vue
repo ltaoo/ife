355491be14fd57f4c3310c9301a223102bc47c90
ltaoo <litaowork@aliyun.com>
2017-08-31 14:23:56 +0800

    【增加】vue 组件库脚手架

---
+++ vueComponents/src/Root.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Input />
+    </div>
+</template>
+
+<script>
+    import '@/assets/reset.css';
+    import '@/assets/common.css';
+
+    import Input from '@/components/Input';
+
+    export default {
+        name: 'app',
+        components: {
+            Input,
+        },
+    };
+</script>
+
+<style scoped>
+</style>
---
+++ vueComponents/src/components/Input.vue
@@ -0,0 +1,12 @@
+<template>
+    <input />
+</template>
+
+<script>
+    export default {
+
+    };
+</script>
+
+<style type="text/css">
+</style>
